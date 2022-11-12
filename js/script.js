30b0978ab757f93d68bacbac056b3e15f58ef24f
Mateusz Kretowski <matkretkurs@gmail.com>
2022-11-12 23:33:23 +0100

    Add titleClickHandler task add class active for targetArticle

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
   });*/
 
   const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log("clickedElement (with plus): ", clickedElement);
 
@@ -22,10 +23,11 @@
       activeArticle.classList.remove("active");
     }
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute("href");
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add("active");
   };
 
   const links = document.querySelectorAll(".titles a");
